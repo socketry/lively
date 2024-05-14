761a653cbe88c309e5d35190477d4ccc34fa7c53
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2024-05-14 16:38:09 +0900

    Fix dependencies.

--- examples/hello-world/application.rb
+++ examples/hello-world/application.rb
@@ -3,13 +3,11 @@
 class HelloWorldView < Live::View
 	def bind(page)
 		super
-		
-		# Update the view on reconnect:
 		self.update!
 	end
 	
 	def render(builder)
-		builder.text("Hello World!")
+		builder.text("Hello RubyKaigi 2024!")
 	end
 end
 
--- examples/hello-world/gems.rb
+++ examples/hello-world/gems.rb
@@ -2,9 +2,3 @@ source "https://rubygems.org"
 
 gem "live"
 gem "lively", path: "../../"
-
-gem "activerecord", "~> 7.1"
-gem "sqlite3", "~> 1.4"
-gem "markly"
-
-gem "async-ollama"
