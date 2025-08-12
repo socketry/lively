135f807159693aa6a4a6fa26c3cbe43ecf2b86d4
jimmy2822 <jimmy2822@gmail.com>
2025-08-12 23:02:27 +0800

    Test RuboCop with bad formatting

--- examples/cs2d/test_rubocop.rb
+++ examples/cs2d/test_rubocop.rb
@@ -1,8 +1,10 @@
-# Test file to verify RuboCop integration
+# frozen_string_literal: true
+
+# Test file to verify RuboCop integration with bad formatting
 
 class TestClass
-  def bad_method
-    x=1+2   # Bad spacing (RuboCop will catch this)
-    puts x
-  end
+	def bad_method
+		x=1+2   # Bad spacing (RuboCop will catch this)
+		puts x
+	end
 end
